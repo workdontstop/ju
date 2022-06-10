abe72601bc2e7e9669009e6da8774ed5bcd2be0e
Tega Ogheneovo <83871801+workdontstop@users.noreply.github.com>
2022-06-10 23:36:00 +0100

    initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,6 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json({ limit: "400mb" }));
 app.use(express.urlencoded({ limit: "300mb" }));
 app.use(cookieParser());
